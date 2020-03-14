62788e87935efd9de43379be294978c0c5759701
Pedro <pedroh1510@hotmail.com>
2020-03-14 20:28:40 -0300

    Updates

---
+++ precache-manifest.129c97ab005c039a650b307a7afd1056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a852978edb89c5b36d8f0dbbba5112c7",
+    "url": "/BinToDec/index.html"
+  },
+  {
+    "revision": "4d70cbaa7fc2aee77835",
+    "url": "/BinToDec/static/css/main.4fa2ec24.chunk.css"
+  },
+  {
+    "revision": "75d6c9001b3499a8d858",
+    "url": "/BinToDec/static/js/2.75c316e6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/BinToDec/static/js/2.75c316e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d70cbaa7fc2aee77835",
+    "url": "/BinToDec/static/js/main.82eba91d.chunk.js"
+  },
+  {
+    "revision": "68f2ed90d042b59ade96",
+    "url": "/BinToDec/static/js/runtime-main.7fb4b8fd.js"
+  }
+]);
